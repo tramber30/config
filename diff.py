c0be02946e9b57ca7e80aa04535211c62cde0935
Nicolas Bertrand <tramber30@gmail.com>
2016-09-29 12:43:31 +0200

    diff.py: remove from repo
    see script repo

--- diff.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-# compatibility workaround for python2.x
-from __future__ import print_function
-import sys
-import os
-
-print("Diff on", sys.argv[5])
-os.system('/usr/bin/meld "%s" "%s" 2>/dev/null' % (sys.argv[2], sys.argv[5]))
