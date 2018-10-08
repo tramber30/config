8abd32f53a4a3b687338f43ba397c0a6b7e7670d
Nico <tramber30@gmail.com>
2018-10-08 09:51:08 +0200

    Enhanced palette_extractor, minor fix
    custom desktop: add gtile basic config
    gitconfig: remove hardcoded params
    palette_extractor: get default profile, basic color display

--- palette_extractor.sh
+++ palette_extractor.sh
@@ -1,1 +1,22 @@
-dconf read /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/palette
+#!/bin/bash
+# Extract palette used by gnome terminal
+
+
+current_profile=`dconf dump /org/gnome/terminal/legacy/profiles:/ | grep -E "^default=" | cut -d "'" -f 2`
+echo -n "Profile: " 
+dconf read /org/gnome/terminal/legacy/profiles:/:${current_profile}/visible-name
+
+echo -n "Palette: " 
+dconf read /org/gnome/terminal/legacy/profiles:/:${current_profile}/palette
+
+echo -n "Background: " 
+dconf read /org/gnome/terminal/legacy/profiles:/:${current_profile}/background-color
+
+echo ""
+
+for color in {0..15}
+do
+        tput setaf $color; echo -n "#$color#" 
+done
+echo ""
+
