89268765fc986d14689e61e348d146ee8941c2d8
Nico <tramber30@gmail.com>
2020-02-03 16:51:54 +0100

    Backup original menu file before patching

--- firefox_desktop/firefox_gnome_menu.sh
+++ firefox_desktop/firefox_gnome_menu.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+BACKUP_DIR=~/.local/patch_backup
+TARGET_FILE=/usr/share/applications/firefox.desktop
+if [ ! -d $BACKUP_DIR ]; then
+    mkdir $BACKUP_DIR
+fi
 
-patch /usr/share/applications/firefox.desktop  firefox.desktop.patch 
+cp $TARGET_FILE $BACKUP_DIR
+patch $TARGET_FILE firefox.desktop.patch 
