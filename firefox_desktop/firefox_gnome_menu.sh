e515e1a8a6e9f02f7b09439758d93b2892d3ed2f
Nico <tramber30@gmail.com>
2020-10-12 17:59:04 +0200

    Update patch to match new firefox default icon context

--- firefox_desktop/firefox_gnome_menu.sh
+++ firefox_desktop/firefox_gnome_menu.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 BACKUP_DIR=~/.local/patch_backup
 TARGET_FILE=/usr/share/applications/firefox.desktop
+
+if [ $(id -u) -ne 0 ]; then
+    echo "admin right necessary"
+    exit
+fi
+
 if [ ! -d $BACKUP_DIR ]; then
     mkdir $BACKUP_DIR
 fi
 
-cp $TARGET_FILE $BACKUP_DIR
-patch $TARGET_FILE firefox.desktop.patch 
+cp $TARGET_FILE $BACKUP_DIR/firefox.desktop_$(date +%d%m%y)
+patch $TARGET_FILE patch_firefox.desktop
