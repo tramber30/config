6cb56b3176861dc9b0a08ba68e0b62d97157b3f2
nico <tramber30@gmail.com>
2021-09-14 13:17:46 +0200

    Fix some identation
    - gitconfig
    - palettte_extractor.sh

--- palette_extractor.sh
+++ palette_extractor.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Extract palette used by gnome terminal
 
-
 current_profile=`dconf dump /org/gnome/terminal/legacy/profiles:/ | grep -E "^default=" | cut -d "'" -f 2`
 echo -n "Profile: " 
 dconf read /org/gnome/terminal/legacy/profiles:/:${current_profile}/visible-name
